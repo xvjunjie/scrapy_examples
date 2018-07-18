b69899b5b8877acd9498a84e42067d2f0e538b46
xvjunjie <1559643591@qq.com>
2018-07-19 07:35:59 +0800

    mongodb

--- tencent/tencent/pipelines.py
+++ tencent/tencent/pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pymongo import MongoClient
-from scrapy import log
+from scrapy import log, Item
 from scrapy.exceptions import DropItem
 
 from tencent import settings
@@ -13,25 +13,47 @@ from tencent import settings
 
 class TencentPipeline(object):
 
-    def __init__(self):
-        client = MongoClient(
-            settings["MONGODB_SERVER"],
-            settings["MONGODB_PORT"]
-        )
-        db = client[settings["MONGODB_DB"]]
-        self.collection = db[settings["MONGODB_COLLECTION"]]
+    # def __init__(self):
+    #     client = MongoClient(
+    #         settings["MONGODB_SERVER"],
+    #         settings["MONGODB_PORT"]
+    #     )
+    #     db = client[settings["MONGODB_DB"]]
+    #     self.collection = db[settings["MONGODB_COLLECTION"]]
+
+    def open_spider(self, spider):
+        db_server = spider.settings.get("MONGODB_SERVER", "localhost")
+        db_port = spider.settings.get("MONGODB_PORT", "27017")
+        db_name = spider.settings.get("MONGODB_DB", "tencent_database")
+        db_collection = spider.settings.get("MONGODB_COLLECTION", "")
+
+        self.db_client = MongoClient(db_server, db_port)
+        self.db = self.db_client[db_name]
+        self.collection = self.db_client[db_collection]
+
+    def close_spider(self, spider):
+        self.db_client.close()
 
     def process_item(self, item, spider):
+        self.insert_db(item)
+        return item
+
+    def insert_db(self, item):
+        '''
+            插入数据
+        :param item:
+        :return:
+        '''
+        if isinstance(item, Item):
+            item = dict(item)
+
         valid = True
         for data in item:
             if not data:
                 valid = False
                 raise DropItem("Missing {0}!".format(data))
         if valid:
-            self.collection.insert(dict(item))
+            # self.db.hr_collection.insert(item)
+            self.collection.insert(item)
             # log.msg("Question added to MongoDB database!",
             #         level=log.DEBUG, spider=spider)
-        return item
-
-
-
