48c392c8d6ad189eacc4b5be4d242a4474bf0d32
xvjunjie <1559643591@qq.com>
2018-07-19 23:00:23 +0800

    .

--- tencent/tencent/spiders/tencent_hr.py
+++ tencent/tencent/spiders/tencent_hr.py
@@ -14,9 +14,9 @@ class TencentHrSpider(scrapy.Spider):
 
     def parse(self, response):
         tr_list = response.xpath("//table[@class='tablelist']/tr")[1:-1]
-        item = TencentItem()
-        for tr in tr_list:
 
+        for tr in tr_list:
+            item = TencentItem()
             item["title"] = tr.xpath("./td[1]/a/text()").extract_first()
             item["position"] = tr.xpath("./td[2]/text()").extract_first()
             item["publish_date"] = tr.xpath("./td[5]/text()").extract_first()
